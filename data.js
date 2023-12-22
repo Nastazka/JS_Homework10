80d35cf31f809a0405dd6372d57671cde9001060
Anastasia <nastazka8@gmail.com>
2023-12-22 12:21:39 +0300

    добавила файл data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+export const dataInfo = `[
+    {
+        "id": "113",
+        "name": "Марго",
+        "breed": "Русская голубая",
+        "age": "8",
+        "image": "https://cdnn21.img.ria.ru/images/07e4/0c/09/1588481441_0:320:3072:2048_1920x0_80_0_0_9146a2aa9cb2feec4fcfcd7611447361.jpg"
+    },
+    {
+        "id": "227",
+        "name": "Ева",
+        "breed": "Сфинкс",
+        "age": "15",
+        "image": "https://www.purina.ru/sites/default/files/2021-06/%D0%A1%D1%84%D0%B8%D0%BD%D0%BA%D1%81%201.jpg"
+    },
+    {
+        "id": "348",
+        "name": "Дориан",
+        "breed": "Британская",
+        "age": "5",
+        "image": "https://s14.stc.all.kpcdn.net/family/wp-content/uploads/2022/08/britanskaya-korotkosherstnaya-koshka-960-540.jpg"
+    },
+    {
+        "id": "462",
+        "name": "Ванесса",
+        "breed": "Сиамская",
+        "age": "12",
+        "image": "https://cdnn21.img.ria.ru/images/156043/00/1560430010_0:81:1200:756_1920x0_80_0_0_8cb0bd7196e68dc3369ea4650d1de9ae.jpg"
+    },
+    {
+        "id": "567",
+        "name": "Барсик",
+        "breed": "Метис",
+        "age": "5",
+        "image": "https://koshka.top/uploads/posts/2021-11/1636688164_2-koshka-top-p-koshki-bengalskaya-koshka-metis-2.jpg"
+    },
+    {
+        "id": "611",
+        "name": "Айс",
+        "breed": "Бурма",
+        "age": "7",
+        "image": "https://egida.by/_pu/23/10431923.jpg"
+    },
+    {
+        "id": "725",
+        "name": "Кенни",
+        "breed": "Манчкин",
+        "age": "9",
+        "image": "https://s16.stc.all.kpcdn.net/family/wp-content/uploads/2022/05/manchkin-960x540-1-960x540.jpg"
+    },
+    {
+        "id": "807",
+        "name": "Марсель",
+        "breed": "Бенгальская",
+        "age": "3",
+        "image": "https://cdn.royalcanin-weshare-online.io/B1Zc8XoBBKJuub5qYbph/v3/bp-lot-2-bengal-couleur-human"
+    }
+]`
